78991056e294f33da8c8816694ee1ecc6a54507c
VuHongSon <vuhongsonjv11@gmail.com>
2017-07-18 11:56:21 +0700

    Fixed. Please check again

--- ex2/ex2/ViewController.swift
+++ ex2/ex2/ViewController.swift
@@ -1,12 +1,12 @@
-//
-//  ViewController.swift
-//  ex2
-//
-//  Created by admin on 7/11/17.
-//  Copyright © 2017 admin. All rights reserved.
-//
-
 import UIKit
+
+extension String{
+    func checkEmail(mail: String) -> Bool{
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: mail)
+    }
+}
 
 class ViewController: UIViewController {
     @IBOutlet weak var btnMale: UIButton!
@@ -16,12 +16,41 @@ class ViewController: UIViewController {
     @IBOutlet weak var txtKata1: UITextField!
     @IBOutlet weak var txtKata2: UITextField!
     @IBOutlet weak var txtEmail: UITextField!
-    @IBOutlet weak var txtDay: UITextField!
-    @IBOutlet weak var txtMonth: UITextField!
-    @IBOutlet weak var txtYear: UITextField!
+    @IBOutlet weak var txtDate: UITextField!
     @IBOutlet weak var txtPhone: UITextField!
     
+    let datePicker = UIDatePicker()
+    
     var gender : String = ""
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        creatDatePicker()
+    }
+    
+    func creatDatePicker(){
+        datePicker.datePickerMode = .date
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(pressDone))
+        toolbar.setItems([doneButton], animated: false)
+        
+        txtDate.inputAccessoryView = toolbar
+        
+        txtDate.inputView = datePicker
+    }
+    
+    func pressDone(){
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .none
+        
+        txtDate.text = dateFormatter.string(from: datePicker.date)
+        self.view.endEditing(true)
+    }
     
     @IBAction func btnCheckMale(_ sender: Any) {
         btnMale.setImage(UIImage.init(named: "check"), for: .normal)
@@ -38,28 +67,25 @@ class ViewController: UIViewController {
     @IBAction func btnRegister(_ sender: Any) {
         let name : String = txtFirstName.text! + " " + txtLastName.text!
         let kata : String = txtKata1.text! + " " + txtKata2.text!
-        let time : String = txtDay.text! + "-" + txtMonth.text! + "-" + txtYear.text!
         let phone : String = txtPhone.text!
+        let date : String = txtDate.text!
         
         let mail : String = txtEmail.text!
-        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}"
-        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
-        let emailChecked = emailTest.evaluate(with: mail)
-        if emailChecked == false {
+        let emailChecked = mail.checkEmail(mail: mail)
+        if !emailChecked {
             let noti = UIAlertController(title: "Warning", message: "Email format isn't correct !", preferredStyle: .alert)
             let btn = UIAlertAction(title: "OK", style: .default, handler: nil)
             noti.addAction(btn)
             present(noti, animated: true, completion: nil)
         }
-        
-        if name == "" || kata == "" || time == "" || phone == "" || mail == "" || gender == "" {
+        else if name == "" || kata == "" || phone == "" || mail == "" || gender == "" || date == "" {
             let noti = UIAlertController(title: "Warning", message: "Please fill all !", preferredStyle: .alert)
             let btn = UIAlertAction(title: "OK", style: .default, handler: nil)
             noti.addAction(btn)
             present(noti, animated: true, completion: nil)
         }
         else{
-            let message = name + "\n" + kata + "\n" + mail + "\n" + time + "\n" + gender + "\n" + phone
+            let message = name + "\n" + kata + "\n" + mail + "\n" + date + "\n" + gender + "\n" + phone
             let registed = UIAlertController(title: "Succeeded" , message: message, preferredStyle: UIAlertControllerStyle.alert)
             registed.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
             self.present(registed, animated: true, completion: nil)
@@ -67,10 +93,8 @@ class ViewController: UIViewController {
             txtLastName.text = ""
             txtKata1.text = ""
             txtKata2.text = ""
-            txtDay.text = ""
-            txtMonth.text = ""
-            txtYear.text = ""
             txtPhone.text = ""
+            txtDate.text = ""
             txtEmail.text = ""
             btnFemale.setImage(UIImage.init(named: "uncheck"), for: .normal)
             btnMale.setImage(UIImage.init(named: "uncheck"), for: .normal)
@@ -78,16 +102,5 @@ class ViewController: UIViewController {
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
 
